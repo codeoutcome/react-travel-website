d962a727c975f083e70389beb66cc2310844cf93
Sam Stimpson <hello+code@samstimpson.com>
2024-03-30 19:42:35 +0000

    playing around with tailwind styling to make cards havent figured responsiveness

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import './input.css';
+import Navbar from '../src/components/Navbar';
+import Footer from '../src/components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div class="shrink-0">
-          <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
-        </div>
-        <div>
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-slate-500">You have a new message!</p>
+      <Navbar />
+
+      <div class="container mx-auto px-4">
+        <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight py-8">Lorem ipsum dolor sit amet</h1>
+        <div class="flex flex-row gap-4">
+          <div class="lg:basis-1/4">
+            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
+            <div class="p-4">
+              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
+            </div>
+          </div>
+          <div class="lg:basis-1/4">
+            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
+            <div class="p-4">
+              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
+            </div>
+          </div>
+          <div class="lg:basis-1/4">
+            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
+            <div class="p-4">
+              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
+            </div>
+          </div>
+          <div class="lg:basis-1/4">
+            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
+            <div class="p-4">
+              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
+            </div>
+          </div>
         </div>
       </div>
+      
+      <Footer />
     </div>
   );
 }
