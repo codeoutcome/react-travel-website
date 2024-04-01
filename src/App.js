235dbe282c7bb736e1992843bb1801538f6751e8
Sam Stimpson <hello+code@samstimpson.com>
2024-04-01 16:07:58 +0100

    added the sponsors component showcase and customer logos

--- src/App.js
+++ src/App.js
@@ -3,51 +3,18 @@ import Footer from '../src/components/Footer';
 import Hero from '../src/components/Hero';
 import Social from '../src/components/Social';
 import Team from '../src/components/Team';
+import CustomerLogos from './components/CustomerLogos';
+import Showcase from './components/Showcase';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      {/* <div class="container mx-auto px-4">
-        <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight py-8">Lorem ipsum dolor sit amet</h1>
-        <div class="flex flex-row gap-4">
-          <div class="lg:basis-1/4">
-            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
-            <div class="p-4">
-              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
-            </div>
-          </div>
-          <div class="lg:basis-1/4">
-            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
-            <div class="p-4">
-              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
-            </div>
-          </div>
-          <div class="lg:basis-1/4">
-            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
-            <div class="p-4">
-              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
-            </div>
-          </div>
-          <div class="lg:basis-1/4">
-            <img class="object-cover w-full max-h-full" src="https://via.placeholder.com/150" alt="placeholder" />
-            <div class="p-4">
-              <h1 class="font-bold text-lg mb-4 mt-5">Lorem ipsum dolor sit amet</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-              <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <Social />
+      <Showcase />
       <Team />
+      <CustomerLogos />
       <Footer />
     </div>
   );
---
+++ src/components/Showcase.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Showcase = () => {
+    return (
+        <section class="block w-full bg-gradient-to-r from-blue-500 to-blue-900">
+            <div class="p-5">
+                Showcase
+            </div>
+        </section>
+    );
+};
+
+export default Showcase;
